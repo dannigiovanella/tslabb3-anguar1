d7530a5ff14df8523ec7e44d496d36f7e53293b0
Daniela Giovanella <dannnigiovanella@gmail.com>
2026-04-24 17:35:46 +0200

    Implementerat routing för de olika undersidorna

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,1 +1,16 @@
+import { Routes } from '@angular/router';
 
+import { HomeComponent } from './pages/home/home.component';
+import { ConverterComponent } from './pages/converter/converter.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'converter', component: ConverterComponent },
+    { path: 'about', component: AboutComponent },
+
+    //404-sida
+    { path: '**', component: NotFoundComponent }
+];
